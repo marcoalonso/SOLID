5dab2ecf3c1645da86b6fd9335e0ab8dd4c22651
Marco Alonso Rodriguez <marcoalonsorota@hotmail.com>
2025-03-28 11:44:16 -0600

    Initial Commit

---
+++ SOLID/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOLID
+//
+//  Created by Marco Alonso Rodriguez on 28/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SOLIDTests/SOLIDTests.swift
@@ -0,0 +1,17 @@
+//
+//  SOLIDTests.swift
+//  SOLIDTests
+//
+//  Created by Marco Alonso Rodriguez on 28/03/25.
+//
+
+import Testing
+@testable import SOLID
+
+struct SOLIDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
